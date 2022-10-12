ff4578707c7fa8574c3e0dd25bce2772373fd953
scalette <furca7e@gmail.com>
2022-10-12 22:14:56 +0200

    feat: tree beginning

---
+++ index.js
@@ -0,0 +1,43 @@
+const { BinarySearchTree } = require('./src/binary-search-tree.js');
+const tree = new BinarySearchTree();
+tree.add(9);
+tree.add(14);
+tree.add(2);
+tree.add(6);
+tree.add(128);
+tree.add(8);
+tree.add(31);
+tree.add(54);
+tree.add(1);
+console.log('_-__-__-__-__-__-__-__-__-__-__-__-_start_-__-__-__-__-__-__-__-__-__-__-__-_')
+console.log(tree.root())
+console.log('_-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-_')
+tree.remove(14);
+tree.remove(8);
+tree.remove(9);
+console.log(tree.root())
+console.log('_-__-__-__-__-__-__-__-__-__-__-__-_finish_-__-__-__-__-__-__-__-__-__-__-__-_')
+console.log(tree.has(14))
+console.log(tree.has(8))
+console.log(tree.has(9))
+console.log(tree.has(2))
+console.log(tree.has(6))
+console.log(tree.has(128))
+console.log(tree.has(31))
+console.log(tree.has(54))
+console.log(tree.has(1))
+
+// tree.add(9);
+// tree.add(14);
+// tree.add(54);
+// tree.add(2);
+// tree.add(6);
+// tree.add(8);
+// tree.add(31);
+// tree.add(1);
+// console.log(tree.root())
+// tree.remove(6);
+// console.log('sip',tree.root())
+// tree.remove(2);
+// console.log(tree.root())
+// console.log(tree.min())
--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,172 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  constructor(){
+    this.tree = new Node(null)
+    this.nodeArray = []
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.tree.data) return null
+    return this.tree 
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addOld_UNORDER_TREE(valueToAdd) {
+    if (!this.tree.data) {
+      this.tree = new Node(valueToAdd)
+      this.nodeArray.push(this.tree)
+    }
+    else{
+      var temp = null
+      while (this.nodeArray.length > 0 && valueToAdd > this.nodeArray[this.nodeArray.length - 1].data){
+        temp = this.nodeArray.pop()
+      }
+      if (temp != null) {
+        temp.right = new Node(valueToAdd)
+        this.nodeArray.push(temp.right)
+      }
+      else{
+        temp = this.nodeArray[this.nodeArray.length - 1]
+        temp.left = new Node(valueToAdd)
+        this.nodeArray.push(temp.left)
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addRecursCall(valueToAdd, tree){
+    if(tree == null){
+      tree = new Node(valueToAdd)
+      return tree
+    }
+    tree.data > valueToAdd ? tree.left = this.addRecursCall(valueToAdd, tree.left) : tree.right = this.addRecursCall(valueToAdd, tree.right)
+    return tree
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(valueToAdd) {
+    if (!this.tree.data) {
+      this.tree = new Node(valueToAdd)
+      return  
+    }
+    this.tree.data > valueToAdd ? this.tree.left = this.addRecursCall(valueToAdd, this.tree.left) : this.tree.right = this.addRecursCall(valueToAdd, this.tree.right)
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(valueToFind) {
+    return this.find(valueToFind) ? true : false
+  }
+
+  find(valueToFind) {
+    if(this.tree.data === null) return null
+    let current = this.tree
+    while (current?.data != valueToFind){
+      if(current?.data === undefined) return null
+      if(current.data > valueToFind) {
+        current = current.left
+      }
+      else{
+        current = current.right
+      }
+    }
+    return current
+  }
+  findNodeForRemove(valueToFind) {
+    if(this.tree.data === null) return null
+    let current = this.tree
+    while (current?.left != valueToFind && current?.right != valueToFind && current?.left?.data != valueToFind && current?.right?.data != valueToFind){
+      if(current?.data === undefined) return null
+      if(current.data > valueToFind) {
+        current = current.left
+      }
+      else{
+        current = current.right
+      }
+    }
+    return current
+  }
+  shuffle(nodeLeft, nodeRight){
+    let connector = nodeLeft
+    while(connector.right != null){
+      connector = connector.right
+    }
+    connector.right = nodeRight
+    console.log('connector................')
+    console.log( connector)
+    console.log('connector................end')
+    return connector
+  }
+  remove(valueToRemove) {
+    //const node = this.find(valueToRemove)
+    if(this.tree.data == valueToRemove){
+      this.tree.data = this.shuffle(this.tree.left, this.tree.right)
+      return
+    }
+    const node = this.findNodeForRemove(valueToRemove)
+    if(node){
+      const nodeRight = node.right
+      const nodeLeft = node.left
+      if(nodeRight.data == valueToRemove){
+        if(nodeRight.left == null && nodeRight.right == null){
+          node.right = null
+          return
+        }
+        if(nodeRight.left == null){
+          node.right = nodeRight.right
+          return
+        }
+        if(nodeRight.right == null){
+          node.right = nodeRight.left
+          return
+        }
+        node.right = this.shuffle(nodeRight.left, nodeRight.right)
+      }
+      if(nodeLeft.data == valueToRemove){
+        if(nodeLeft.left == null && nodeLeft.right == null){
+          node.left = null
+          return
+        }
+        if(nodeLeft.left == null){
+          node.left = nodeLeft.right 
+          return
+        }
+        if(nodeLeft.right == null){
+          node.left = nodeLeft.left 
+          return
+        }
+        node.left = this.shuffle(nodeLeft.left, nodeLeft.right)
+      }
+      // const nodeRight = node.right
+      // const nodeLeft = node.left
+      // if(!nodeRight || !nodeLeft){
+      //   node.data = nodeLeft || nodeRight || null 
+      //   return
+      // }
+      // let nodeConnector = nodeLeft.right
+      // while(nodeConnector != null){
+      //   nodeConnector = nodeConnector.right
+      // }
+      // nodeConnector = nodeRight
+      // node.data = nodeLeft
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let min = this.tree.left
+    while(min.left != null){
+      min = min.left
+    }
+    return min.data
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let max = this.tree.right
+    while(max.right != null){
+      max = max.right
+    }
+    return max.data
   }
 }
 
